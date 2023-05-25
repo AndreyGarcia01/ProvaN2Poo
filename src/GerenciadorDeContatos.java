1d607014e7109a63fe7117c72e29e6d2faf2a3ab
Andrey Garcia <andreygarcia204@gmail.com>
2023-05-25 19:26:34 -0300

    Prova n2 Poo

--- src/GerenciadorDeContatos.java
+++ src/GerenciadorDeContatos.java
@@ -1,17 +1,30 @@
 import java.io.*;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class GerenciadorDeContatos implements Serializable {
-    private Map<String, String> contatos;
+    private Map<String, Contato> contatos;
 
     public GerenciadorDeContatos() {
         contatos = new HashMap<>();
     }
 
     public void adicionarContato(String nome, String numeroTelefone) {
-        contatos.put(nome, numeroTelefone);
+        contatos.put(nome, new Contato(nome, numeroTelefone));
+    }
+
+    public void adicionarContatoComData(String nome, String numeroTelefone, LocalDate dataNascimento) {
+        contatos.put(nome, new ContatoComData(nome, numeroTelefone, dataNascimento));
+    }
+
+    public void adicionarContatoAmigo(String nome, String numeroTelefone, String apelido) {
+        contatos.put(nome, new ContatoAmigo(nome, numeroTelefone, apelido));
+    }
+
+    public void adicionarContatoTrabalho(String nome, String numeroTelefone, String empresa) {
+        contatos.put(nome, new ContatoTrabalho(nome, numeroTelefone, empresa));
     }
 
     public void removerContato(String nome) {
@@ -20,8 +33,8 @@ public class GerenciadorDeContatos implements Serializable {
 
     public void buscarContato(String nome) {
         if (contatos.containsKey(nome)) {
-            String numeroTelefone = contatos.get(nome);
-            System.out.println("Número de telefone de " + nome + ": " + numeroTelefone);
+            Contato contato = contatos.get(nome);
+            System.out.println("Número de telefone de " + nome + ": " + contato.getNumeroTelefone());
         } else {
             System.out.println("Contato não encontrado: " + nome);
         }
@@ -29,15 +42,15 @@ public class GerenciadorDeContatos implements Serializable {
 
     public void listarContatos() {
         System.out.println("Lista de contatos:");
-        for (Map.Entry<String, String> contato : contatos.entrySet()) {
+        for (Map.Entry<String, Contato> contato : contatos.entrySet()) {
             String nome = contato.getKey();
-            String numeroTelefone = contato.getValue();
+            String numeroTelefone = contato.getValue().getNumeroTelefone();
             System.out.println("Nome: " + nome + ", Número de telefone: " + numeroTelefone);
         }
     }
 
     public static void main(String[] args) {
-        GerenciadorDeContatos gerenciador = null;
+        GerenciadorDeContatos gerenciador = new GerenciadorDeContatos();
         Scanner scanner = new Scanner(System.in);
 
         boolean executando = true;
@@ -45,11 +58,14 @@ public class GerenciadorDeContatos implements Serializable {
         while (executando) {
             System.out.println("Escolha uma opção:");
             System.out.println("1 - Adicionar contato");
-            System.out.println("2 - Remover contato");
-            System.out.println("3 - Buscar contato");
-            System.out.println("4 - Listar contatos");
-            System.out.println("5 - Salvar contatos");
-            System.out.println("6 - Carregar contatos");
+            System.out.println("2 - Adicionar contato com data de nascimento");
+            System.out.println("3 - Adicionar contato amigo");
+            System.out.println("4 - Adicionar contato do trabalho");
+            System.out.println("5 - Remover contato");
+            System.out.println("6 - Buscar contato");
+            System.out.println("7 - Listar contatos");
+            System.out.println("8 - Salvar contatos");
+            System.out.println("9 - Carregar contatos");
             System.out.println("0 - Sair");
 
             int opcao = scanner.nextInt();
@@ -65,20 +81,51 @@ public class GerenciadorDeContatos implements Serializable {
                     System.out.println("Contato adicionado com sucesso!");
                     break;
                 case 2:
+                    System.out.print("Digite o nome do contato: ");
+                    String nomeAdicionarComData = scanner.nextLine();
+                    System.out.print("Digite o número de telefone: ");
+                    String numeroTelefoneAdicionarComData = scanner.nextLine();
+                    System.out.print("Digite a data de nascimento (yyyy-MM-dd): ");
+                    String dataNascimentoStr = scanner.nextLine();
+                    LocalDate dataNascimento = LocalDate.parse(dataNascimentoStr);
+                    gerenciador.adicionarContatoComData(nomeAdicionarComData, numeroTelefoneAdicionarComData, dataNascimento);
+                    System.out.println("Contato com data de nascimento adicionado com sucesso!");
+                    break;
+                case 3:
+                    System.out.print("Digite o nome do contato amigo: ");
+                    String nomeAdicionarAmigo = scanner.nextLine();
+                    System.out.print("Digite o número de telefone: ");
+                    String numeroTelefoneAdicionarAmigo = scanner.nextLine();
+                    System.out.print("Digite o apelido: ");
+                    String apelido = scanner.nextLine();
+                    gerenciador.adicionarContatoAmigo(nomeAdicionarAmigo, numeroTelefoneAdicionarAmigo, apelido);
+                    System.out.println("Contato amigo adicionado com sucesso!");
+                    break;
+                case 4:
+                    System.out.print("Digite o nome do contato do trabalho: ");
+                    String nomeAdicionarTrabalho = scanner.nextLine();
+                    System.out.print("Digite o número de telefone: ");
+                    String numeroTelefoneAdicionarTrabalho = scanner.nextLine();
+                    System.out.print("Digite o nome da empresa: ");
+                    String empresa = scanner.nextLine();
+                    gerenciador.adicionarContatoTrabalho(nomeAdicionarTrabalho, numeroTelefoneAdicionarTrabalho, empresa);
+                    System.out.println("Contato do trabalho adicionado com sucesso!");
+                    break;
+                case 5:
                     System.out.print("Digite o nome do contato a ser removido: ");
                     String nomeRemover = scanner.nextLine();
                     gerenciador.removerContato(nomeRemover);
                     System.out.println("Contato removido com sucesso!");
                     break;
-                case 3:
+                case 6:
                     System.out.print("Digite o nome do contato a ser buscado: ");
                     String nomeBuscar = scanner.nextLine();
                     gerenciador.buscarContato(nomeBuscar);
                     break;
-                case 4:
+                case 7:
                     gerenciador.listarContatos();
                     break;
-                case 5:
+                case 8:
                     try {
                         salvarContatos(gerenciador);
                         System.out.println("Contatos salvos com sucesso!");
@@ -86,7 +133,7 @@ public class GerenciadorDeContatos implements Serializable {
                         System.out.println("Erro ao salvar contatos: " + e.getMessage());
                     }
                     break;
-                case 6:
+                case 9:
                     try {
                         gerenciador = carregarContatos();
                         System.out.println("Contatos carregados com sucesso!");
@@ -124,5 +171,62 @@ public class GerenciadorDeContatos implements Serializable {
         in.close();
         fileIn.close();
         return gerenciador;
+    }
+}
+
+class Contato implements Serializable {
+    private String nome;
+    private String numeroTelefone;
+
+    public Contato(String nome, String numeroTelefone) {
+        this.nome = nome;
+        this.numeroTelefone = numeroTelefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNumeroTelefone() {
+        return numeroTelefone;
+    }
+}
+
+class ContatoComData extends Contato {
+    private LocalDate dataNascimento;
+
+    public ContatoComData(String nome, String numeroTelefone, LocalDate dataNascimento) {
+        super(nome, numeroTelefone);
+        this.dataNascimento = dataNascimento;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+}
+
+class ContatoAmigo extends Contato {
+    private String apelido;
+
+    public ContatoAmigo(String nome, String numeroTelefone, String apelido) {
+        super(nome, numeroTelefone);
+        this.apelido = apelido;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+}
+
+class ContatoTrabalho extends Contato {
+    private String empresa;
+
+    public ContatoTrabalho(String nome, String numeroTelefone, String empresa) {
+        super(nome, numeroTelefone);
+        this.empresa = empresa;
+    }
+
+    public String getEmpresa() {
+        return empresa;
     }
 }
